10e5804fdf265fe9586d793335d8f68c2bb3331f
andrey.vokin <andrey.vokin@jetbrains.com>
2012-07-09 19:55:54 +0400

    Interaction between main loop and parser started

--- src/main/java/com/avokin/dict/converter/Main.java
+++ src/main/java/com/avokin/dict/converter/Main.java
@@ -4,19 +4,27 @@ import com.avokin.dict.converter.db.DbHelper;
 import com.avokin.dict.converter.model.DictionaryItem;
 import com.avokin.dict.converter.parser.MuellerDictionaryParser;
 
+import java.io.IOException;
+
 /**
  * User: Andrey.Vokin
  * Date: 7/9/12
  */
 public class Main {
-    public static void main(String[] args) {
-        MuellerDictionaryParser parser = new MuellerDictionaryParser();
+    public static final String DICTIONARY_PATH = "data/SmallMueller7GPL.koi";
+
+    public static void main(String[] args) throws IOException {
+        final MuellerDictionaryParser parser = new MuellerDictionaryParser(DICTIONARY_PATH);
 
         DbHelper.createDb();
         DbHelper.openDb();
         while (parser.ready()) {
             DictionaryItem di = parser.getNextItem();
+            if (di == null) {
+                break;
+            }
             DbHelper.addDictionaryItem(di);
+            System.out.println(di.word);
 
         }
         DbHelper.closeDb();
--- src/main/java/com/avokin/dict/converter/parser/MuellerDictionaryParser.java
+++ src/main/java/com/avokin/dict/converter/parser/MuellerDictionaryParser.java
@@ -2,16 +2,26 @@ package com.avokin.dict.converter.parser;
 
 import com.avokin.dict.converter.model.DictionaryItem;
 
+import java.io.*;
+
 /**
  * User: Andrey.Vokin
  * Date: 7/9/12
  */
 public class MuellerDictionaryParser {
-    public boolean ready() {
-        return false;
+    private BufferedReader reader;
+
+    public MuellerDictionaryParser(final String dictionaryPath) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(dictionaryPath);
+        reader = new BufferedReader(new InputStreamReader(fis));
     }
 
-    public DictionaryItem getNextItem() {
+    public boolean ready() throws IOException {
+        return reader.ready();
+    }
+
+    public DictionaryItem getNextItem() throws IOException {
+        final String line = reader.readLine();
         return null;
     }
 }
