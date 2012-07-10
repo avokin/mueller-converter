2374c2696292dff80b43f68681933d8a6d550a91
Andrey Vokin <andrey.vokin@gmail.com>
2012-07-10 17:13:50 +0400

    Writing to SQLite db implemented

--- src/main/java/com/avokin/dict/converter/Main.java
+++ src/main/java/com/avokin/dict/converter/Main.java
@@ -5,28 +5,30 @@ import com.avokin.dict.converter.model.DictionaryItem;
 import com.avokin.dict.converter.parser.MuellerDictionaryParser;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * User: Andrey.Vokin
  * Date: 7/9/12
  */
 public class Main {
-    public static final String DICTIONARY_PATH = "data/SmallMueller7GPL.koi";
+  public static final String DICTIONARY_PATH = "data/SmallMueller7GPL.koi";
 
-    public static void main(String[] args) throws IOException {
-        final MuellerDictionaryParser parser = new MuellerDictionaryParser(DICTIONARY_PATH);
-
-        DbHelper.createDb();
-        DbHelper.openDb();
-        while (parser.ready()) {
-            DictionaryItem di = parser.getNextItem();
-            if (di == null) {
-                break;
-            }
-            DbHelper.addDictionaryItem(di);
-            System.out.println(di.word);
-
+  public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+    final MuellerDictionaryParser parser = new MuellerDictionaryParser(DICTIONARY_PATH);
+    try {
+      DbHelper.createDb();
+      while (parser.ready()) {
+        DictionaryItem di = parser.getNextItem();
+        if (di == null) {
+          break;
         }
-        DbHelper.closeDb();
+        DbHelper.addDictionaryItem(di);
+        System.out.println(di.word + "  [" + di.getTranscription() + "] " + di.getTranslations());
+
+      }
+    } finally {
+      DbHelper.closeDb();
     }
+  }
 }
--- src/main/java/com/avokin/dict/converter/db/DbHelper.java
+++ src/main/java/com/avokin/dict/converter/db/DbHelper.java
@@ -1,25 +1,43 @@
 package com.avokin.dict.converter.db;
 
 import com.avokin.dict.converter.model.DictionaryItem;
+import org.sqlite.SQLiteConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * User: Andrey.Vokin
  * Date: 7/9/12
  */
 public class DbHelper {
-    public static void createDb() {
+    private static Connection connection;
 
+    public static void createDb() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        SQLiteConfig config = new SQLiteConfig();
+        connection = DriverManager.getConnection("jdbc:sqlite:sample.db", config.toProperties());
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("drop table if exists words");
+        statement.executeUpdate("drop table if exists android_metadata");
+
+        statement.executeUpdate("create table android_metadata(locale string)");
+        statement.executeUpdate("insert into android_metadata(locale) values('en_US')");
+
+        statement.executeUpdate("create table words(word string, transcription string, translations string)");
     }
 
-    public static void openDb() {
-
+    public static void closeDb() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
     }
 
-    public static void closeDb() {
-
-    }
-
-    public static void addDictionaryItem(DictionaryItem dictionaryItem) {
-
+    public static void addDictionaryItem(DictionaryItem dictionaryItem) throws SQLException {
+        Statement insertStatement = connection.createStatement();
+        insertStatement.executeUpdate(String.format("insert into words(word, transcription, translations) values('%s', '%s', '%s')", dictionaryItem.getWord(), dictionaryItem.getTranscription(), dictionaryItem.getTranslations()));
     }
 }
